de946389b6cd26f1aad1382cfa8b4007b621ae84
macapalacios <maca.palacios.311@gmail.com>
2021-05-23 19:12:56 -0300

    Add express-limit

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const limit = require('express-limit').limit;
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', limit({
+  max:    5,        // 5 requests
+  period: 60 * 1000 // per minute (60 seconds)
+}), function(req, res, next) {
   res.render('index', { product_1: 'Kapping-Gel' , price_1: 1500,
                         product_2: 'Semi-Permanente', price_2: 175});
 });
-
 
 module.exports = router;
