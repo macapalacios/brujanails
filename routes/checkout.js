f731728d156e6db925cb04e5b9eecdedf8b33e40
macapalacios <maca.palacios.311@gmail.com>
2021-05-23 18:54:29 -0300

    DomPurify

--- routes/checkout.js
+++ routes/checkout.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+// DomPurify
+const createDOMPurify = require('dompurify');
+const { JSDOM } = require('jsdom');
+const window = new JSDOM('').window;
+const DOMPurify = createDOMPurify(window);
+// Fin DomPurify
+
 require('dotenv').config()
 
 const mercadopago = require('mercadopago');
@@ -12,27 +19,30 @@ mercadopago.configure({
 /* POST checkout listing. */
 router.post('/', function(req, res, next) {
   
-let preference = {
-  items: [
-    {
-      title: req.body.title,
-      quantity: 1,
-      currency_id: 'ARS',
-      unit_price: parseFloat(req.body.price)
-    }
-  ],
-  back_urls: {
-      "success": "https://localhost:3443",
-      "failure": "https://localhost:3443",
-      "pending": "https://localhost:3443"
-  },
-  auto_return: 'approved'
+  let price = DOMPurify.sanitize(req.body.price);
+  let title = DOMPurify.sanitize(req.body.title);
+  
+  let preference = {
+    items: [
+      {
+        title: title,
+        quantity: 1,
+        currency_id: 'ARS',
+        unit_price: parseFloat(price)
+      }
+    ],
+    back_urls: {
+        "success": "https://localhost:3443",
+        "failure": "https://localhost:3443",
+        "pending": "https://localhost:3443"
+    },
+    auto_return: 'approved'
 };
 
 mercadopago.preferences.create(preference)
  .then(function(response){
 
-  res.redirect(response.body.init_point);
+  res.redirect(DOMPurify.sanitize(response.body.init_point));
 
  }).catch(function(err){
    console.log(err)
